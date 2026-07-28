5b3410190a20b95f4b4d4daeffcb8eb7a9accdf8
Martina <acetomartina141@gmail.com>
2026-07-28 17:27:51 +0200

    Initial commit

---
+++ src/main/java/com/martina/caf_fapi/CafFapiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.martina.caf_fapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafFapiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafFapiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/martina/caf_fapi/CafFapiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martina.caf_fapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafFapiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

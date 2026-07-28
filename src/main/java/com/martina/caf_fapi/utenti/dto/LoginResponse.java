d98a93ea70aec36f2b2b976fe110013952421622
Martina <acetomartina141@gmail.com>
2026-07-28 19:37:29 +0200

    feat: aggiungere LoginResponse

--- src/main/java/com/martina/caf_fapi/utenti/dto/LoginResponse.java
+++ src/main/java/com/martina/caf_fapi/utenti/dto/LoginResponse.java
@@ -1,4 +1,37 @@
 package com.martina.caf_fapi.utenti.dto;
 
+import com.martina.caf_fapi.utenti.Ruolo;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class LoginResponse {
+
+    private String accessToken;
+
+    @Builder.Default
+    private String tokenType = "Bearer";
+
+    private LocalDateTime expiresAt;
+
+    private Long utenteId;
+
+    private String nome;
+
+    private String cognome;
+
+    private String email;
+
+    private Ruolo ruolo;
+
+    private String urlImmagineProfilo;
 }
